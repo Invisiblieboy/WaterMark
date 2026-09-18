b9088516d8a8697021e904bf7fa52092d09b9d06
Yarik <sasala.m.mal@gmail.com>
2026-09-18 16:31:48 +0300

    add base files

---
+++ itmo_bmp/BMPImage.cpp
@@ -0,0 +1,10 @@
+#include "BMPImage.h"
+
+namespace itmo_bmp{
+
+    status_code BMPImage::load(char* file_name){
+        return status_code::kOK;
+    };
+
+
+};
---
+++ itmo_bmp/BMPImage.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+
+namespace itmo_bmp{
+    class Pixel {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+    };
+
+    enum status_code{
+        kOK, kFileNotFound, kUnknownError
+    };
+
+
+    class BMPImage {
+    public:
+        BMPImage();
+        ~BMPImage();
+
+        // Публичное API класса
+        status_code load(char* file_name);
+        status_code save(char* file_name);
+
+        // Точечная работа с пикселями
+        Pixel get_pixel_color(int x, int y);
+        status_code set_pixel_color(int x, int y, Pixel* pixel);
+    };
+}
