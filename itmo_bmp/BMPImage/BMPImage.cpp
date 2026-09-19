0e217d312a06860eac1071b66e3d25236f878527
Yarik <sasala.m.mal@gmail.com>
2026-09-19 11:59:21 +0300

    feat: добавить функционал2

--- itmo_bmp/BMPImage/BMPImage.cpp
+++ itmo_bmp/BMPImage/BMPImage.cpp
@@ -1,21 +1,44 @@
 #include "BMPImage.h"
-#include <iostream>
 
 namespace itmo_bmp {
-
-BMPImage::BMPImage() = default;
+BMPImage::BMPImage() : width_(-1), height_(-1), image_(nullptr) {}
 BMPImage::~BMPImage() {
-    // TODO: освободить image, когда начнёшь его выделять
-}
+    if (image_ != nullptr){
+        delete image_;
+    }
 }
 
-int main(){
-    itmo_bmp::BMPImage img;
-
-    char* file_name;
-    std::cin >> file_name;
-    std::cout << img.load(file_name);
-
-    
-    return 0;
+status_code BMPImage::load(const char* file_name) {
+    return kOK;  // заглушка
 }
+
+Pixel* BMPImage::memory_preparation_(const uint32_t len) {
+    Pixel* ptr = new Pixel[len];
+    return ptr;
+}
+
+Pixel* BMPImage::get_pixel_color(const uint32_t x, const uint32_t y) {
+    return image_ + (y * width_ + x);
+}
+
+status_code BMPImage::gen_empty_sheet(const uint32_t width, const uint32_t height, const uint8_t color_depth){
+    if (width==0 || height == 0){
+        return status_code::kInvalidArg;
+    }
+    image_ = memory_preparation_(width * height);
+
+    width_ = width;
+    height_ = height;
+    color_depth_ = color_depth;
+    return status_code::kOK;
+}
+
+status_code BMPImage::set_pixel_color(uint32_t x, uint32_t y, const Pixel* pixel){
+    if (x >= width_ || y >= height_){
+        return status_code::kInvalidArg;
+    }
+    *(image_ + (y * width_ + x)) = *pixel;
+
+    return status_code::kOK;
+}
+} // namespace itmo_bmp
--- itmo_bmp/BMPImage/BMPImage.h
+++ itmo_bmp/BMPImage/BMPImage.h
@@ -3,13 +3,13 @@
 
 namespace itmo_bmp{
     class Pixel {
-        uint8_t r;
-        uint8_t g;
-        uint8_t b;
+        uint8_t r = 0;
+        uint8_t g = 0;
+        uint8_t b = 0;
     };
 
     enum status_code{
-        kOK, kFileNotFound, kUnknownError
+        kOK, kFileNotFound, kPermissionDenied, kInvalidArg, kUnknownError
     };
 
     class BMPImage {
@@ -18,14 +18,22 @@ namespace itmo_bmp{
         ~BMPImage();
 
         // Публичное API класса
-        status_code load(char* file_name);
-        status_code save(char* file_name);
+        status_code load(const char* file_name);
+        status_code save(const char* file_name);
+
+        status_code gen_empty_sheet(const uint32_t width, const uint32_t height, const uint8_t color_depth);
 
         // Точечная работа с пикселями
-        Pixel get_pixel_color(int x, int y);
-        status_code set_pixel_color(int x, int y, Pixel* pixel);
+        Pixel* get_pixel_color(const uint32_t x, const uint32_t y);
+        status_code set_pixel_color(const uint32_t x, const uint32_t y, const Pixel* pixel);
+
     
     private:
-        Pixel** image = nullptr;
+        uint32_t width_;
+        uint32_t height_;
+        uint8_t color_depth_;
+        Pixel* image_ ;
+
+        Pixel* memory_preparation_(const uint32_t len);
     };
-};
+} // namespace itmo_bmp
